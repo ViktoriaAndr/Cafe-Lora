9c15b2c819334add6954446553bd2c34132dc8e1
Viktoria Andrascikova <viktoria.andrascikova@gmail.com>
2022-04-30 11:46:24 +0200

    hamburger toggle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './style.css';
 
 console.log('funguju!');
+
+const dropDown = document.querySelector('nav');
+
+const toggleNav = () => {
+  dropDown.classList.toggle('nav-closed');
+};
+
+const hamburger = document.querySelector('#nav-btn');
+
+hamburger.addEventListener('click', toggleNav);
+
+const closeNav = () => {
+  dropDown.classList.add('nav-closed');
+};
+
+const linkList = document.querySelectorAll('a');
+
+for (let i = 0; i < linkList.length; i++) {
+  linkList[i].addEventListener('click', closeNav);
+}
