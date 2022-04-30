4c171a2e227c18f62229c69df04c37c20d8cc66d
Viktoria Andrascikova <viktoria.andrascikova@gmail.com>
2022-04-30 16:33:56 +0200

    dynamicka komponenta napoje

--- src/Drink/index.js
+++ src/Drink/index.js
@@ -2,7 +2,8 @@ import './style.css';
 import { Layer } from '../Layer';
 
 export const Drink = (props) => {
-  const { id, name, ordered, image, layers } = props;
+  const { id, name, image, layers } = props;
+  let { ordered } = props;
   const drinkElm = document.createElement('div');
   drinkElm.classList.add('drink');
   drinkElm.innerHTML = `
@@ -14,15 +15,25 @@ export const Drink = (props) => {
     </div>
     <div class="drink__info">
       <h3>${name}</h3>
+      </div>
   </div>
   <div class="drink__controls">
     <button class="order-btn">Objednat</button>
-  </div>
 </div>
 `;
   const drinkinfoElm = drinkElm.querySelector('.drink__info');
   for (let i = 0; i < layers.length; i++) {
     drinkinfoElm.innerHTML += Layer(layers[i]);
   }
+  const orderBtn = drinkElm.querySelector('.order-btn');
+
+  const drinkSelect = drinkElm.querySelector('.drink__cup');
+
+  const drink = () => {
+    const selected = drinkSelect.classList.toggle('drink__cup--selected');
+    orderBtn.textContent = selected ? 'Zrušit' : 'Objednat';
+  };
+
+  orderBtn.addEventListener('click', drink);
   return drinkElm;
 };
