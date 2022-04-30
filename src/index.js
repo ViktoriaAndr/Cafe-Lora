69c56e8798f207a0193f7bc95fa92b4d168a2578
Viktoria Andrascikova <viktoria.andrascikova@gmail.com>
2022-04-30 17:11:48 +0200

    seznam napoju

--- src/index.js
+++ src/index.js
@@ -22,26 +22,26 @@ import { Drink } from './Drink';
 //   drinkinfoElm.innerHTML += Layer(layers[i]);
 // }
 
-const drinkExample = {
-  id: 'romano',
-  name: 'Romano',
-  ordered: false,
-  layers: [
-    {
-      color: '#fbdf5b',
-      label: 'banan',
-    },
-    {
-      color: '#613916',
-      label: 'espresso',
-    },
-  ],
-  image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/romano.png',
-};
+// const drinkExample = {
+//   id: 'romano',
+//   name: 'Romano',
+//   ordered: false,
+//   layers: [
+//     {
+//       color: '#fbdf5b',
+//       label: 'banan',
+//     },
+//     {
+//       color: '#613916',
+//       label: 'espresso',
+//     },
+//   ],
+//   image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/romano.png',
+// };
 
-const newdrinkElm = Drink(drinkExample);
-const drinkslistElm = document.querySelector('.drinks-list');
-drinkslistElm.appendChild(newdrinkElm);
+// const newdrinkElm = Drink(drinkExample);
+// const drinkslistElm = document.querySelector('.drinks-list');
+// drinkslistElm.appendChild(newdrinkElm);
 
 const dropDown = document.querySelector('nav');
 
@@ -63,13 +63,49 @@ for (let i = 0; i < linkList.length; i++) {
   linkList[i].addEventListener('click', closeNav);
 }
 
-// const orderBtn = document.querySelector('.order-btn');
+const drinks = [
+  {
+    id: 'cappuccino',
+    name: 'Cappuccino',
+    ordered: false,
+    layers: [
+      {
+        color: '#feeeca',
+        label: 'mléčná pěna',
+      },
+      {
+        color: '#fed7b0',
+        label: 'teplé mléko',
+      },
+      {
+        color: '#613916',
+        label: 'espresso',
+      },
+    ],
+    image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/cappuccino.png',
+  },
+  {
+    id: 'romano',
+    name: 'Romano',
+    ordered: false,
+    layers: [
+      {
+        color: '#fbdf5b',
+        label: 'citrón',
+      },
+      {
+        color: '#613916',
+        label: 'espresso',
+      },
+    ],
+    image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/romano.png',
+  },
+];
 
-// const drinkSelect = document.querySelector('.drink__cup');
+drinks.forEach((item) => {
+  document.querySelector('.drinks-list').appendChild(Drink(item));
+});
 
-// const drink = () => {
-//   const selected = drinkSelect.classList.toggle('drink__cup--selected');
-//   orderBtn.textContent = selected ? 'Zrušit' : 'Objednat';
-// };
-
-// orderBtn.addEventListener('click', drink);
+//const newdrinkElm = Drink(drinkExample);
+// const drinkslistElm = document.querySelector('.drinks-list');
+// drinkslistElm.appendChild(newdrinkElm)
